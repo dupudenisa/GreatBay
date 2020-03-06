1940c4665bc3b8bf6014310a3cf0e93ca52a7b21
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-03-06 14:13:11 -0600

    skeleton and json added

---
+++ Great_Bay.js
@@ -0,0 +1,36 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql")
+
+
+
+
+const auction = () => {
+    inquirer
+        .prompt(
+            {
+                type:"input",
+                message:"Would ",
+
+            },{
+
+            },{
+
+            }
+        )
+}
+
+const post = () => {
+
+}
+
+const bid = () => {
+
+}
+
+const updateBid = () => {
+
+}
+
+const createItem = () => {
+
+}
