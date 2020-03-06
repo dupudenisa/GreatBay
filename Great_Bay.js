b388f5b8982cd74ea2c12d343a5ef34653dd6d3b
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-03-06 14:33:24 -0600

    prompts are done

--- Great_Bay.js
+++ Great_Bay.js
@@ -8,24 +8,67 @@ const auction = () => {
     inquirer
         .prompt(
             {
-                type:"input",
-                message:"Would ",
-
-            },{
-
-            },{
+                type: "list",
+                message: "Are you trying to make a POST or make a BID",
+                choices: ["POST", "BID"],
+                name: "pOrB",
 
             }
         )
-}
+        .then(function(res) {
+            if (res.pOrb === "POST") {
 
-const post = () => {
+                inquirer
+                    .prompt(
+                        {
+                            type: "input",
+                            message: "What is the item that you would like to post",
+                            name: "itemName",
+                        }, {
+                            type: "input",
+                            message: "In what catagory would you like to post your item",
+                            name: "catagory",
+                        }, {
+                            type: "input",
+                            message: "What would you like to set the satring bid at",
+                            name: "startPrice",
+                        }
+                    )
+                    .then(function(res) {
 
-}
+                        const post = () => {
+        
+                        }
+                    })
+            }
+            else if (res.pOrb === "BID") {
 
-const bid = () => {
+                inquirer
+                    .prompt(
+                        {
+                            type: "input",
+                            message: "What is the item that you are looking to bid on.",
+                            name: "itemBid",
+                        }, {
+                            type: "input",
+                            message: "How much are you looking to bit.",
+                            name: "bid",
+                        }
+                    )
+                    .then(function(res) {
 
-}
+                        const bid = () => {
+
+                        }
+                    })
+
+                
+            }
+        })
+
+};
+
+
 
 const updateBid = () => {
 
